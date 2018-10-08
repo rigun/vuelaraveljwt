8424531ef2a79a47e2b2403bb3bfce1508380790
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-10-08 22:49:36 +0700

    init

---
+++ resources/js/components/Layout/dashboardLayout.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="content">
+       <div class="container">
+           <div class="card">
+               <div class="card-content">
+                   Selamat Datang di website SMPN 4 Depok
+                   <router-link v-bind:to="{path: '/login'}"><i class="fa fa-home m-r-10" aria-hidden="true"></i>  <span>Login</span> </router-link>
+               </div>
+           </div>
+       </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
