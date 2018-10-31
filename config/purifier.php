1e3c6637c4c644ddafafc2bb6881ca5e5ab55221
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-10-31 09:22:52 +0700

    bugfix

--- config/purifier.php
+++ config/purifier.php
@@ -25,7 +25,7 @@ return [
         'default' => [
             'HTML.Doctype'             => 'HTML 4.01 Transitional',
             'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],table[style|border],tr[style],td[style],th[style],tbody[style]',
-            'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,border-collapse,width,',
+            'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align,border-collapse,width',
             'AutoFormat.AutoParagraph' => true,
             'AutoFormat.RemoveEmpty'   => true,
         ],
